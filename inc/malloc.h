0c9c9f864322849f2e1b5200f45232c23c08ebfb
aautin <alexandreautin.aa@gmail.com>
2026-05-24 18:59:30 +0200

    ignore .vscode config, creating main structures

--- inc/malloc.h
+++ inc/malloc.h
@@ -2,7 +2,46 @@
 
 // LIBC headers
 #include <stddef.h>
+#include <stdbool.h>
+#include <unistd.h>
 
+enum e_fixed_size
+{
+	TINY  = 2048,
+	SMALL = TINY * 128,
+};
+
+// Structs
+typedef struct s_space
+{
+	size_t          size;
+	struct s_space *previous;
+	bool 		    taken;
+}	t_space;
+
+typedef struct s_block
+{
+	struct s_block *next;
+}	t_block;
+
+typedef struct s_large_block
+{
+	size_t                size;
+	struct s_large_block *next;
+}	t_large_block;
+//
+// One single space is placed right after the block metadata (start and end 16-bytes aligned)
+// No size (already in block metadata), no previous pointer and no taken boolean (a large block is always alone and taken)
+//
+
+typedef struct s_blocks
+{
+	t_block       *tinies;
+	t_block       *smalls;
+	t_large_block *larges;
+}	t_blocks;
+
+// Functions
 void *malloc(size_t size);
 void free(void *ptr);
 void *realloc(void *ptr, size_t size);
--- src/malloc.c
+++ src/malloc.c
@@ -2,11 +2,55 @@
 
 #include <unistd.h>
 
+static t_blocks g_blocks = {NULL, NULL, NULL, 0};
+
+size_t get_page_size(void)
+{
+	static size_t page_size = 0;
+
+	if (page_size == 0)
+	{
+		page_size = (size_t) sysconf(_SC_PAGESIZE);
+	}
+	return (page_size);
+}
+
+size_t align_on_16(size_t size)
+{
+	if (size % 16 == 0)
+		return (size);
+	return ((size / 16 + 1) * 16);
+}
+
+size_t align_on_page(size_t size)
+{
+	size_t page_size;
+
+	page_size = get_page_size();
+	if (size % page_size == 0)
+	{
+		return (size);
+	}
+	return ((size / page_size + 1) * page_size);
+}
+
+size_t get_block_size(enum e_fixed_size fixed_size)
+{
+	//
+	// aligned block metadata + 100 spaces (metadata + aligned data)
+	//
+	return align_on_page(align_on_16(sizeof(t_block)) + 100 * align_on_16(sizeof(t_space)) + 100 * fixed_size);
+}
+
+size_t get_block_size(size_t custom_size)
+{
+	//
+	// block metadata + 
+	//
+	return align_on_16(sizeof(t_large_block)) + align_on_16(custom_size);
+}
+
 void *malloc(size_t size)
 {
-	write(STDOUT_FILENO, "My malloc called\n", 18);
-
-	(void) size;
-	
 	return (NULL);
 }
